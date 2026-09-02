f1ef1cc8edf729c26a57b841379e1ea2d64a3581
Hitesh Choukiker <121677727+Hitesh029@users.noreply.github.com>
2026-09-03 05:01:47 +0530

    Time: 0 ms (100%), Space: 21.6 MB (76.45%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,35 +4,32 @@ public:
                                 vector<int>& newInterval) {
 
         vector<vector<int>> ans;
-        bool inserted = false;
 
-        for(auto interval : intervals) {
+        int i = 0;
+        int n = intervals.size();
 
-            if(interval[1] < newInterval[0]) {
-
-                ans.push_back(interval);
-            }
-            else if(interval[0] > newInterval[1]) {
-
-                if(!inserted) {
-                    ans.push_back(newInterval);
-                    inserted = true;
-                }
-
-                ans.push_back(interval);
-            }
-            else {
-
-                newInterval[0] =
-                    min(newInterval[0], interval[0]);
-
-                newInterval[1] =
-                    max(newInterval[1], interval[1]);
-            }
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
         }
 
-        if(!inserted)
-            ans.push_back(newInterval);
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+
+            newInterval[0] =
+                min(newInterval[0], intervals[i][0]);
+
+            newInterval[1] =
+                max(newInterval[1], intervals[i][1]);
+
+            i++;
+        }
+
+        ans.push_back(newInterval);
+
+        while(i < n) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
 
         return ans;
     }
